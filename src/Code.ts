8957bbc96080fe86711477cee79245a8897a1a7f
Rytis Cepulis <cepRytis@gmail.com>
2022-01-30 17:54:30 +0200

    filter1

--- src/Code.ts
+++ src/Code.ts
@@ -5,11 +5,28 @@ const fromHashesA1Notation = "D12:D";
 function myFunction(){
   const idsTemp = ["10HqqvSphHj4vS_aCOPn2GYg3o-1dpKuAFpvBmllb6-o", "1AZMcjGTK5NGcd1L0fSe9z4vnhFotnzseE3AtwkW6X8Q", "1jp6oGM7F-edX42zvs6CGpCmdhuPsfWi5fGVEmjjS1zA"];
 
+  // const tempResult = filterWalletsActiveInSpecifiedAmountUniqueTokens(idsTemp, 2, 3);
   const tempResult = getWalletTokensMap(idsTemp);
+
 
   tempResult.forEach((value, key) => {
     Logger.log(key + ": " + value);
   });
+}
+
+function filterWalletsActiveInSpecifiedAmountUniqueTokens(
+ sheetsIdPool: string[], minAmountBought: number, maxAmountBought: number): Map<string, number>{
+  const walletsTokensMap = getWalletTokensMap(sheetsIdPool);
+  let result = new Map<string, number>();
+
+  walletsTokensMap.forEach((tokensHashes, walletHash) => {
+    const nmOfTokens = tokensHashes.length;
+    if(nmOfTokens >= minAmountBought && nmOfTokens <= maxAmountBought){
+      result.set(walletHash, nmOfTokens);
+    }
+  });
+
+  return result;
 }
 
 function getWalletTokensMap(sheetsIdPool:string[]): Map<string, string[]>{
@@ -31,11 +48,11 @@ function getWalletTokensMap(sheetsIdPool:string[]): Map<string, string[]>{
 
 function addUniqueTokenCreateWalletIfDoesntExist(tokenHash: string, walletHash: string, walletTokensMap: Map<string, string[]>){
   const tokens = walletTokensMap.get(walletHash);
-  if (tokens && !tokens.includes(tokenHash)){
-    tokens.push(tokenHash);
-  }
-  else{
+  if (!tokens){
     walletTokensMap.set(walletHash, [tokenHash])
+  }
+  else if(!tokens.includes(tokenHash)){
+    tokens.push(tokenHash);
   }
 }
 
