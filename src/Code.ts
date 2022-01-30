cd811180eae177593291be892609bd595a2f9d03
Rytis Cepulis <cepRytis@gmail.com>
2022-01-30 18:02:17 +0200

    from hash sometimes is empty

--- src/Code.ts
+++ src/Code.ts
@@ -5,8 +5,8 @@ const fromHashesA1Notation = "D12:D";
 function myFunction(){
   const idsTemp = ["10HqqvSphHj4vS_aCOPn2GYg3o-1dpKuAFpvBmllb6-o", "1AZMcjGTK5NGcd1L0fSe9z4vnhFotnzseE3AtwkW6X8Q", "1jp6oGM7F-edX42zvs6CGpCmdhuPsfWi5fGVEmjjS1zA"];
 
-  // const tempResult = filterWalletsActiveInSpecifiedAmountUniqueTokens(idsTemp, 2, 3);
-  const tempResult = getWalletTokensMap(idsTemp);
+  const tempResult = filterWalletsActiveInSpecifiedAmountUniqueTokens(idsTemp, 3, 3);
+  // const tempResult = getWalletTokensMap(idsTemp);
 
 
   tempResult.forEach((value, key) => {
@@ -33,13 +33,14 @@ function getWalletTokensMap(sheetsIdPool:string[]): Map<string, string[]>{
   let walletTokensMap = new Map<string, string[]>();
   
   sheetsIdPool.forEach(id => {
-
     const sheet = SpreadsheetApp.openById(id);
     const tokenHash: string = sheet.getRange(tokenHashA1Notation).getValue();
     const newWalletsHashes: string[] = sheet.getRange(fromHashesA1Notation).getValues().map((w: string[]) => w[0]);
 
     newWalletsHashes.forEach(wallet => {
-      addUniqueTokenCreateWalletIfDoesntExist(tokenHash, wallet, walletTokensMap);
+      if(wallet != ""){
+        addUniqueTokenCreateWalletIfDoesntExist(tokenHash, wallet, walletTokensMap);
+      }
     });
   });
 
